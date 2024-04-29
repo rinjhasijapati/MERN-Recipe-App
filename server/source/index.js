ab31257a1ccb8aba6d051b605a852e06eda94c25
rinjhasijapati <aastha_sijapati@yahoo.com>
2024-04-29 21:48:51 +0545

    routes

--- server/source/index.js
+++ server/source/index.js
@@ -2,10 +2,14 @@ import express from "express";
 import cors from 'cors'
 import mongoose from 'mongoose'
 
+import {userRouter} from './routes/users.js'
+
 const app = express()
 
 app.use(express.json()); //when you send data from  frontend to backend, it will be converted into json
 app.use(cors());
+
+app.use("/auth", userRouter);
 
 mongoose.connect("");
 
--- server/source/routes/users.js
+++ server/source/routes/users.js
@@ -1,3 +1,11 @@
 import express from 'express'
-import jwt from 'jsonwebtoken';
+import jwt from 'jsonwebtoken'
 import bcrypt from 'bcrypt'
+
+const router = express.Router()
+
+
+
+
+
+export {router as userRouter};
